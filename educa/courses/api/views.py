75075d0753fd4cbf80b49c308050aee5aab902d2
Todor Toshev <t.toshev95@gmail.com>
2021-11-21 19:25:02 +0200

    Added an API for serializing subject, course and module; views for subject list and details, BasicAuthentication for CourseEnrollView and urlpatterns.

---
+++ educa/courses/api/views.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+from ..models import Subject
+from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Course
+from rest_framework.authentication import BasicAuthentication
+
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'courses',
     'students',
     'embed_video',
+    'rest_framework',
 
 
     'django.contrib.admin',
@@ -136,3 +137,10 @@ LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
