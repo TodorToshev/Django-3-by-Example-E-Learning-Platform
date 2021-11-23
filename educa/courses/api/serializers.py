a7b0e11324a9a4b51b430cc1bb226f44a380c24a
Todor Toshev <t.toshev95@gmail.com>
2021-11-23 22:22:22 +0200

    Added permissions for enrolled students and serializers for course contents, urlpatterns and viewset for viewing contents.

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -3,7 +3,7 @@
 from rest_framework import serializers
 from ..models import Subject
 from ..models import Course, Module
-
+from ..models import Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -20,6 +20,35 @@ class ModuleSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+    
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
 
     class Meta:
         model = Course
